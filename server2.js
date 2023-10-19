3a47c9421cb5fa2c57723c48bd4ce33fe43297bc
Sujay Ghosh <sujayghosh@Sujays-MacBook-Air.local>
2023-10-20 03:31:59 +0530

    Sujay's Changes

--- server2.js
+++ server2.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8005; // You can change the port number as need
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-        origin: ['http://localhost:3000'],  // Allow requests from this origin
+        origin: ['https://www.zeebanglamuktomancho.com'],  // Allow requests from this origin
          credentials: true, // Allow credentials (e.g., cookies, authorization headers)
        }));
 
